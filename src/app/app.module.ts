155e835d3fdcd5e70b8eb0ea79bd6a0fe1cfc9e2
jonatan <jonatan.puche@soprasteria.com>
2022-07-11 13:34:30 +0200

    feat(sanitas-front): add form and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  form: FormGroup = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rememberMe: [false],
+    },
+    { updateOn: 'submit' }
+  );
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
+  onSubmit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) console.log('OK');
+  }
 }
